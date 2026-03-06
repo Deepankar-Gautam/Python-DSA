cde8597586dc5cb089944f9f61ee233ab5ac641f
Deepankar <deepankar.gautam1012@gmail.com>
2026-03-06 22:22:13 +0530

    Day-1.py

---
+++ Day1.py
@@ -0,0 +1,37 @@
+"""
+===============================================
+      Data Structures and Algorithms
+===============================================
+
+
+Data Structure: is a way of organising, storing, and managing data in memory so it can be accessed and modified efficiently.
+
+Software: is a set of executable instructions that process input, manipulate data, and produce output.
+
+Software (contain programs) = Program (becomes process while execution) = Process
+
+note: "Process" word only applicable for the programs running in memory.
+
+"""
+
+#=========================================================
+# Extration of last digit 
+#=========================================================
+
+# n = int (input ("Enter your number : "))
+# num = n
+
+# def last_digit(num):
+#     while num != 0:
+#         l = num % 10     # give last digit
+#         num = num // 10  # remove last digit from number
+#         print (l) 
+
+# last_digit(n)
+# print (n)
+
+
+
+
+
+
